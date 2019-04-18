e32d0ec9f45d50264a14c6af73704f4d94884542
yangeee <534650291@qq.com>
2019-04-19 01:36:14 +0800

    n-button-group组件制作圆角

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import Vue from 'vue/dist/vue'
 import Button from './button'
 import Icon from "./icon"
+import buttonGroup from "./n-button-group"
 
 Vue.component('n-icon', Icon)
 
@@ -8,7 +9,8 @@ Vue.component('n-icon', Icon)
 new Vue({
     el: '#app',
     components:{
-        "n-button": Button
+        "n-button": Button,
+        "n-button-group": buttonGroup
     },
     data(){
         return{
---
+++ src/n-button-group.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="n-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: '',
+}
+</script>
+
+
+<style lang="scss" scoped>
+.n-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  > .n-button {
+    border-radius: 0;
+    margin-left: -1px;
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
