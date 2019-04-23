0b69daad1ec5d016ff50902dd4070a92014f67ed
yangeee <534650291@qq.com>
2019-04-23 14:35:05 +0800

    使用chai-spies监听点击事件触发回调

--- src/app.js
+++ src/app.js
@@ -21,7 +21,9 @@ new Vue({
 }) 
 
 import chai from 'chai'
+import spies from 'chai-spies'
 
+chai.use(spies)
 const expect = chai.expect
 
 {
@@ -31,9 +33,78 @@ const expect = chai.expect
             icon: 'settings'
         }
     })
-    button.$mount('#test')
+    button.$mount()
     let useElement = button.$el.querySelector('use')
     let href = useElement.getAttribute('xlink:href')
     expect(href).to.eq('#i-settings')
+    button.$el.remove()
+    button.$destroy()
 
+}
+
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon: 'settings',
+            loading: true
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    button.$el.remove()
+    button.$destroy()
+}
+
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon: 'settings',
+            iconPosition: 'left'
+
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    button.$el.remove()
+    button.$destroy()
+}
+
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    button.$el.remove()
+    button.$destroy()
+}
+// mock
+{
+    const Constructor = Vue.extend(Button)
+    const gButton = new Constructor({
+        propsData:{
+            icon: 'settings',
+        }
+    })
+    gButton.$mount()
+    let spy = chai.spy(function(){console.log(1)})
+    gButton.$on('click',spy)
+    let button = gButton.$el
+    button.click()
 }
