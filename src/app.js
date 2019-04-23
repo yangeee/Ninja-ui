c7b6e383eb886c6b42216eb827bbe59a904b85d9
yangeee <534650291@qq.com>
2019-04-23 13:37:05 +0800

    初始化chai单元测试

--- src/app.js
+++ src/app.js
@@ -19,3 +19,21 @@ new Vue({
     }
 
 }) 
+
+import chai from 'chai'
+
+const expect = chai.expect
+
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon: 'settings'
+        }
+    })
+    button.$mount('#test')
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings')
+
+}
