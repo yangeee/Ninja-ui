a7b4e9db7e8baea3c324f741586b127db51f857c
yangeee <534650291@qq.com>
2019-04-27 17:42:45 +0800

    Input组件基本样式完成

--- src/input.vue
+++ src/input.vue
@@ -1,16 +1,70 @@
 <template>
-  <div class="wrapper">
-    <input type="text">
-  </div>
+  <div class="wrapper" :class="{'error':error}">
+    <input :value="value" :disabled="disabled" :readonly="readonly" type="text">
+    <template v-if='error'>
+    <icon name='error' class='icon icon-error'></icon>
+    <span class="errorMessage">{{error}}</span>
+   </template>
+   </div>
 </template>
 
 
 <script>
+import Icon from './icon'
 export default {
-  name: '',
+  name: 'NinjaInput',
+  components: {
+    icon: Icon
+  },
+  props: {
+    value: {
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String
+    }
+  }
 }
 </script>
 
 
-<style scoped>
+<style lang="scss" scoped>
+$height: 32px;
+$border-color: #999;
+$font-size: 12px;
+$border-color-hover: #666;
+$box-shadow-color: rgba(0, 0, 0, 0.5);
+$red: #f1453d;
+.wrapper {
+  font-size: $font-size;
+  display: inline-flex;
+  align-items: center;
+  > :not(:last-child){
+    margin-right: .5em;
+  }
+  > input { height: 32px; border: 1px solid $border-color; border-radius: 4px; padding: 0 8px; font-size: inherit;
+    &:hover { border-color: $border-color-hover; }
+    &:focus { box-shadow: inset 0 1px 3px $box-shadow-color; outline: none; }
+    &[disabled],
+    &[readonly] { border-color: #bbb; color: #aaa; cursor: not-allowed; } }
+  &.error {
+    > input {
+      border-color: $red;
+    }
+  }
+  .icon-error{
+    fill: $red;
+  }
+  .errorMessage{
+    color: $red;
+  }
+}
 </style>
